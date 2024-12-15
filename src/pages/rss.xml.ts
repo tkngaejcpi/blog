46aaa68c22d0053cb6622c48df3a075512703ea8
Riley Chang <rileycki3333@gmail.com>
2024-12-15 17:29:00 +0800

    use description instead full-text in rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,24 +1,10 @@
 import rss from '@astrojs/rss';
-
-import sanitizeHtml from 'sanitize-html';
+import { getCollection } from 'astro:content';
 
 import { config } from '@config';
-import { type Model as Frontmatter } from '@models/Post';
 
-interface Posts {
-	[x: string]: {
-		frontmatter: Frontmatter;
-		compiledContent: () => Promise<string>;
-	};
-}
-
-export const GET = async (context) => {
-	const posts: Posts = import.meta.glob('../data/posts/**/*.md', {
-		eager: true,
-	});
-
-	const postIds = Object.keys(posts);
-	const strip = (postId: string) => postId.slice(14, postId.length - 3);
+export const GET = async (context: { site: string }) => {
+	const posts = await getCollection('posts');
 
 	return rss({
 		title: config.name,
@@ -28,17 +14,12 @@ export const GET = async (context) => {
 		stylesheet: '/rss.xsl',
 		trailingSlash: false,
 
-		items: await Promise.all(
-			postIds.map(async (postId) => {
-				const { title, createdDate } = posts[postId].frontmatter;
+		items: posts.map((post) => ({
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.createdDate,
 
-				return {
-					title,
-					pubDate: createdDate,
-					link: `/posts/${strip(postId)}`,
-					content: sanitizeHtml(await posts[postId].compiledContent()),
-				};
-			}),
-		),
+			link: `${context.site}posts/${post.id}`,
+		})),
 	});
 };
