8c8718179711d3ba3a351d83522ca181750c0310
Riley Chang <rileycki3333@gmail.com>
2024-12-15 17:22:23 +0800

    allow post to have description frontmatter

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -4,8 +4,9 @@ export const schema = z.object({
 	title: z.string(),
 	createdDate: z.date(),
 
+	description: z.string().optional(),
 	tags: z.array(z.string()).default([]),
 	copyrightReserved: z.boolean().default(false),
 });
 
-export type Model = z.infer<typeof schema>;
+export type Schema = z.infer<typeof schema>;
